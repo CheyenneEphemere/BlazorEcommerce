c804845a1e332e23aea348ea906c2b52c504899e
CheyenneEphemere <cheyenne.ephemere@outlook.com>
2022-10-19 14:51:42 +0200

    Add project files.

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,42 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext : DbContext
+
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Banana",
+                    Description = "Fruit Selected and stored fresh",
+                    ImageUrl = "https://m.media-amazon.com/images/I/31zxVniGbKL.jpg",
+                    Price = 0.59m
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Medium Hass Avocado",
+                    Description = "Vegetable Grown in Mexico or California",
+                    ImageUrl = "https://m.media-amazon.com/images/I/81aqzdQPufL._SL1469_.jpg",
+                    Price = 1.49m
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "Feta Crumbles",
+                    Description = "365 by Whole Foods Market",
+                    ImageUrl = "https://m.media-amazon.com/images/I/71WAUJ5chvL._SL1500_.jpg",
+                    Price = 3.99m
+                });
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Server/Migrations/20221019091316_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "Fruit Selected and stored fresh", "https://m.media-amazon.com/images/I/31zxVniGbKL.jpg", 0.59m, "Banana" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Vegetable Grown in Mexico or California", "https://m.media-amazon.com/images/I/81aqzdQPufL._SL1469_.jpg", 1.49m, "Medium Hass Avocado" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "365 by Whole Foods Market", "https://m.media-amazon.com/images/I/71WAUJ5chvL._SL1500_.jpg", 3.99m, "Feta Crumbles" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
